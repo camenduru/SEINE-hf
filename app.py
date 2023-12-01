806cef4f96775c45927dd9c59a2992aaab415858
Xinyuan Chen <xinyuanc91@users.noreply.huggingface.co>
2023-12-01 06:12:21 +0000

    Update app.py

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ with gr.Blocks(css='style.css') as demo:
     gr.Markdown("<font color=red size=10><center>SEINE: Image-to-Video generation</center></font>")
     gr.Markdown(
         """<div style="text-align:center">
-        [Arxiv Report](https://arxiv.org/abs/2310.20700) | [Project Page](https://vchitect.github.io/SEINE-project/) | [Github](https://github.com/Vchitect/SEINE)
+        [Arxiv Report](https://arxiv.org/abs/2310.20700) | [Project Page](https://vchitect.github.io/SEINE-project/) | [Github](https://github.com/Vchitect/SEINE)</div>
         """
     )
     with gr.Column(elem_id="col-container"):
